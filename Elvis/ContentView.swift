40b507e74d487d282252cbc9d96ffa2d7b7e82aa
fvj5054 <fvj5054@gmail.com>
2025-02-04 10:59:49 -0800

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by Francisco Valencia on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State private var message = "Peace"
+    @State private var imageName = "peacesign"
+    
+    var body: some View {
+        VStack {
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                }
+            }
+            .tint(.purple)
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElvisTests/ElvisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisTests.swift
+//  ElvisTests
+//
+//  Created by Student1 on 1/29/25.
+//
+
+import Testing
+@testable import Elvis
+
+struct ElvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
